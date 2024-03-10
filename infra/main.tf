b6409924048d44fdc1a950398afd2d44caefb17b
Matthew Catania <matthewcatania@hotmail.com>
2024-03-10 11:23:38 +0100

    rename st

--- infra/main.tf
+++ infra/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 module "st_terraform" {
   source                        = "./modules/storage-account"
-  name                          = "stmcataniaterraform${var.env}${var.loc}"
+  name                          = "stmcataniatf${var.env}${var.loc}"
   location                      = azurerm_resource_group.rg.location
   resource_group_name           = azurerm_resource_group.rg.name
   account_tier                  = "Standard"
