fa763982acb792c26fe6a1b5eeffc9432d63c00b
Matthew Catania <matthewcatania@hotmail.com>
2024-03-10 11:29:27 +0100

    move state file to storage account

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = ">=3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-mcatania-test-westeu"
+    storage_account_name = "stmcataniatftestwesteu"
+    container_name       = "tf-mcatania-container"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
