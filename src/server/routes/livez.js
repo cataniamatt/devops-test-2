674c893daf28ca4ad5d278b0b8ba7f2af2b0a8e2
Matthew Catania <matthewcatania@hotmail.com>
2024-03-10 14:24:45 +0100

    output hostname

--- src/server/routes/livez.js
+++ src/server/routes/livez.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const os = require("os");
 const router = express.Router();
-
 
 router.get("/", async (req, res) => {
     try {
+        const hostname = os.hostname(); // Get the hostname
         res.send({
             ts: new Date(),
-            mgs: 'Everything looks fine here....'
+            hostname: hostname, // Include the hostname in the response
+            msg: 'Everything looks fine here....'
         });
     } catch (error) {
         res.send(error);
